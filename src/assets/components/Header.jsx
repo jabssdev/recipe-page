03405691f907f71fcc94bc4e8522126410cbc767
JabssDev <jordanburgoss23@gmail.com>
2024-08-31 17:52:52 -0500

    feat: :sparkles: End ingredients section

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -2,16 +2,16 @@ import omelette from "../images/image-omelette.jpeg";
 
 export const Header = () => {
 	return (
-		<header className="mb-5">
+		<header className="">
 			<img src={omelette} alt="Plate of a simple omelet" />
-			<section className="px-8 py-9">
+			<section className="px-8 pt-9">
 				<h1 className="text-4xl font-young-serif mb-5 text-ct-stone-900">Simple Omelette Recipe</h1>
-				<p className="mb-6 text-ct-stone-600 font-[400]">
+				<p className="mb-7 text-ct-stone-600 font-[400]">
 					An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked to perfection, optionally filled with your
 					choice of cheese, vegetables, or meats.
 				</p>
 				<div className="py-6 pl-6 pr-14 bg-ct-rose-50 rounded-xl">
-					<h2 className="text-xl font-[700] mb-3 text-ct-rose-800">Preparation Time</h2>
+					<h3 className="text-xl font-[700] mb-3 text-ct-rose-800">Preparation Time</h3>
 					<ul className="ml-9 marker:text-ct-rose-800 marker:text-[0.775rem] leading-[26px]">
 						<li className="li-center">
 							<p className="text-ct-stone-600">
--- src/assets/components/overview-recipe/Ingredients.jsx
+++ src/assets/components/overview-recipe/Ingredients.jsx
@@ -1,7 +1,25 @@
 export const Ingredients = () => {
 	return (
-		<>
-			<h1>Ingredients</h1>
-		</>
+		<section className="px-8 pt-7 mb-5">
+			<h2 className="text-2xl font-young-serif text-ct-rose-800 mb-3">Ingredients</h2>
+			<ul className="ml-9 marker:text-ct-rose-800 marker:text-[0.775rem] leading-7">
+				<li className="li-center">
+					<p className="text-ct-stone-600">2-3 large eggs</p>
+				</li>
+				<li className="li-center">
+					<p className="text-ct-stone-600">Salt, to taste</p>
+				</li>
+				<li className="li-center">
+					<p className="text-ct-stone-600">Pepper, to taste</p>
+				</li>
+				<li className="li-center">
+					<p className="text-ct-stone-600">1 tablespoon of butter or oil</p>
+				</li>
+				<li className="li-center">
+					<p className="text-ct-stone-600">Optional fillings: cheese, diced vegetables, cooked meats, herbs</p>
+				</li>
+			</ul>
+			<hr className="border-ct-stone-600 opacity-25 mt-8" />
+		</section>
 	);
 };
